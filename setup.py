eefc1405e5c1b6bf0ab4d32a62673f2d7f6a97bf
Pradeep8802 <ai21btech11013@iith.ac.in>
2024-07-26 18:08:18 +0530

    init added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fluxtune',
-    version='0.1',
+    version='0.1.1',
     description='Flux tune is a library intended to implemnet various online machine learning algorithms and agents',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
